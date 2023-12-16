cda38c6755e38d1d542dab4484e4a6e6e4fe1af8
Tony Godwin <tgodwin108@yahoo.com>
2023-12-16 17:10:14 -0600

    Rails for Beginners Part 5: Routes and Route Types https://gorails.com/episodes/rails-for-beginners-part-5-routes-and-route-types

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,8 @@
+class AboutController < ApplicationController
+
+    def index
+        # because there is no code
+        # Rails looks for a VIEW
+        #   - views/about/index.html
+    end
+end
