e975f1675d6d68684686732ad30f21306b026987
Tony Godwin <tgodwin108@yahoo.com>
2023-12-16 17:23:22 -0600

    Rails for Beginners Part 6: The Root Route https://gorails.com/episodes/rails-for-beginners-part-6-the-root-route

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,8 @@
+class MainController < ApplicationController
+
+    def index
+        # because there is no code
+        # Rails looks for a VIEW
+        #   - views/main/index.html.erb
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
   # GET /about
-  get "about", to: "about#index"
+  get "/about", to: "about#index"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get "/", to: "main#index"
+  # Rails has special syntax for the "root route"
+  root to: "main#index"
+
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
