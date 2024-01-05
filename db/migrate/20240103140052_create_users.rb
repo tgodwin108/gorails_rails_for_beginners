21fbea08ac4bdc98118b64c929d527b780d3d476
Tony Godwin <tgodwin108@yahoo.com>
2024-01-05 14:13:25 -0600

    I wanted to be more specific about using a change() method in a migration.

--- db/migrate/20240103140052_create_users.rb
+++ db/migrate/20240103140052_create_users.rb
@@ -1,11 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
 
-  # Reversible migrations are migrations that know how 
-  # to go +down+ for you. You simply supply the +up+ logic, 
-  # and the Migration system figures out how to execute the 
-  # down commands for you.
-  # 
-  # To define a reversible migration, define the +change+ method
+  # Normally you define two methods in a migration. These are
+  # the +up+ method and the +down+ method. With these two methods
+  # you can "migrate" the DB to any configuration / version.
+  
+  # There are also "Reversible Migrations" which are migrations
+  # that only define a +change+ method. The +change+ method
+  # contains the commands you would have put in the +up+ method.
+  # With a "Reversable Migration" the system figures out how to 
+  # execute the +down+ commands for you.
+
   def change
     create_table :users do |t|
       t.string :email
