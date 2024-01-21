e97ff4f3cc1885e1000d9c96a7c3a5a9d6f3d7cd
Tony Godwin <tgodwin108@yahoo.com>
2024-01-21 12:21:19 -0600

    Rails for Beginners Part 14: Handling Sign Up Errors https://gorails.com/episodes/rails-for-beginners-part-14-handling-sign-up-errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,28 @@ class RegistrationsController < ApplicationController
         @user = User.new
     end
 
+    def create
+        # render plain: "Thanks!\n\t#{params[:user]}"
+
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to root_path, notice: "Successfully created account"
+        else
+            # flash[:alert] = "OOPS - something went wrong"
+            render :new
+        end
+    end
+
+private
+
+    # Add some security before inserting data into the database
+    #    `@user = User.new(params[:user])` => TOO DANGERIOUS
+    #
+    # The `params` object has the `.require()` and `.permit()` methods.
+    # We call these to preform some validation on the `params` we got 
+    # from the network.
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
